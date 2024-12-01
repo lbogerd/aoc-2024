89f5a5bafd861902664e2ad41f9607c93c5bc6bf
Lucas Bogerd <l.bogerd@protonmail.com>
2024-12-02 00:36:39 +0100

    Add part 2 functionality and remove unused files

--- day-1/code.ts
+++ day-1/code.ts
@@ -1,3 +1,4 @@
+// PART 1
 const data = await Deno.readFile("day-1/input.txt");
 
 const parsed = new TextDecoder("utf-8").decode(data);
@@ -29,5 +30,16 @@ for (let i = 0; i < leftArray.length; i++) {
 }
 
 const totalDifference = differences.reduce((prev, next) => prev + next, 0);
-
 console.log(totalDifference);
+
+// PART 2
+const similarities = [];
+for (let i = 0; i < leftArray.length; i++) {
+  const left = leftArray[i];
+  const similarity = left * rightArray.filter((n) => n === left).length;
+
+  similarities.push(similarity);
+}
+
+const totalSimilarity = similarities.reduce((prev, next) => prev + next, 0);
+console.log(totalSimilarity);

be3dd79977d35b1b3fc0e6b11f047182dbd4eca5
Lucas Bogerd <l.bogerd@protonmail.com>
2024-12-02 00:28:40 +0100

    init advent of code x deno

---
+++ day-1/code.ts
@@ -0,0 +1,33 @@
+const data = await Deno.readFile("day-1/input.txt");
+
+const parsed = new TextDecoder("utf-8").decode(data);
+
+const leftArray = [];
+const rightArray = [];
+
+const lines = parsed.split("\n");
+
+for (const line of lines) {
+  const [left, right] = line.split("   ");
+
+  leftArray.push(parseInt(left));
+  rightArray.push(parseInt(right));
+}
+
+leftArray.sort();
+rightArray.sort();
+
+const differences = [];
+for (let i = 0; i < leftArray.length; i++) {
+  const left = leftArray[i];
+  const right = rightArray[i];
+
+  let difference = left - right;
+  if (difference < 0) difference = -difference;
+
+  differences.push(difference);
+}
+
+const totalDifference = differences.reduce((prev, next) => prev + next, 0);
+
+console.log(totalDifference);
